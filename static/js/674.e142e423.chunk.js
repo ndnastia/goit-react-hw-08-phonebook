16e16fb72378d3e2ad1f3155068173641f7b45a6
ndnastia <116677721+ndnastia@users.noreply.github.com>
2023-11-11 01:58:00 +0000

    Deploying to gh-pages from @ ndnastia/goit-react-hw-08-phonebook@11ccccb770270b9f59e3052740b86aea4effa522 🚀

---
+++ static/js/674.e142e423.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[674],{8674:function(e,r,n){n.r(r),n.d(r,{default:function(){return o}});var t="HomePage_banner__JZczy",a=n(184),o=function(){return(0,a.jsxs)("div",{className:t,children:[(0,a.jsx)("h1",{children:"Hi! I'm your private phonebook."}),(0,a.jsx)("p",{children:"To add contact please Login or Register"})]})}}}]);
+//# sourceMappingURL=674.e142e423.chunk.js.map
